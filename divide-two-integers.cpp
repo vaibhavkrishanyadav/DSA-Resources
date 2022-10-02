9cf7cd5f59b94b53ab4aaac5b37ae7307122e508
Sneha Pandey <55934654+snehapandey2000@users.noreply.github.com>
2022-10-03 00:41:24 +0530

    Update divide-two-integers.cpp

--- divide-two-integers.cpp
+++ divide-two-integers.cpp
@@ -1,47 +1,20 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if (divisor == INT_MIN) {
-            if (dividend == INT_MIN) {
-                return 1;
-            } else {
-                return 0;
+        if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
+        }
+        long dvd = labs(dividend), dvs = labs(divisor), ans = 0;
+        int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
+        while (dvd >= dvs) {
+            long temp = dvs, m = 1;
+            while (temp << 1 <= dvd) {
+                temp <<= 1;
+                m <<= 1;
             }
+            dvd -= temp;
+            ans += m;
         }
-        
-        if (dividend == INT_MIN) {
-            if (divisor == -1) {
-                return INT_MAX;
-            } 
-            if (divisor > 0) {
-                return -1+divide(dividend+divisor, divisor);
-            } else if (divisor < 0) {
-                return 1+divide(dividend-divisor, divisor);
-            }
-        }
-        
-        int sign = 1;
-        if ((dividend >0 && divisor <0) || (dividend <0 && divisor >0)) {
-            sign = -1;
-        }
-        
-        //dividend = dividend > 0 ? dividend : -dividend;
-        //divisor = divisor > 0 ? divisor : -divisor;
-        dividend=abs(dividend);
-        divisor=abs(divisor);
-        int remainder = dividend, quotient = 0;
-        while (remainder >= divisor) {
-            int shift = 0;
-            while ((divisor<<shift) <= remainder && (divisor<<shift)>0 ) {
-                ++shift;
-            }
-            --shift;
-            quotient += (1<<shift);
-            remainder -= (divisor<<shift);
-        }
-        if (sign < 0) quotient = -quotient;
-        return quotient;
-        
+        return sign * ans;
     }
-    
 };
